004ac27a23e38e4e7cb3c2d07b4862f86e5fc47c
Biellei <gabrielmsouza13@gmail.com>
2025-04-04 00:28:38 +0000

    terminei de colocar as zonas da abilidade

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,9 +3,10 @@
 #define TAMANHO 10  // Define o tamanho do tabuleiro
 #define NAVIO 3      // Representação do navio
 #define AGUA 0       // Representação da água
+#define AREA_AFETADA_PELA_ABILIDADE 5 // Representação da abilidade
 
 void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-    for (int i = 0; i < TAMANHO; i++) {
+   for (int i = 0; i < TAMANHO; i++) {
         for (int j = 0; j < TAMANHO; j++) {
             tabuleiro[i][j] = AGUA;
         }
@@ -34,7 +35,40 @@ void posicionarNavios(int tabuleiro[TAMANHO][TAMANHO], int x1, int y1, int taman
     }
 }
 
-void exibirCoordenadas(int x1, int y1, int tamanho1, int x2, int y2, int tamanho2, int x3, int y3, int tamanho3, int x4, int y4, int tamanho4) {
+void posicionarAbilidades(int tabuleiro[TAMANHO][TAMANHO], int x5, int y5, int tamanho5, int x6, int y6, int tamanho6, int x7, int y7, int tamanho7, int x8, int y8, int tamanho8, int x9, int y9, int tamanho9, int x10, int y10, int tamanho10, int x11, int y11, int tamanho11) {
+    // Posiciona Abilidade cone
+    for (int i = 0; i < tamanho5; i++) {
+        tabuleiro[x5][y5 + i] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+   for (int i = 0; i < tamanho6; i++) {
+        tabuleiro[x6][y6 + i] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+    for (int i = 0; i < tamanho7; i++) {
+        tabuleiro[x7][y7 + i] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+    // Posiciona Abilidade octaedro
+    for (int i = 0; i < tamanho8; i++) {
+        tabuleiro[x8][y8 + i] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+    for (int i = 0; i < tamanho9; i++) {
+        tabuleiro[x9 + i][y9] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+    // Posiciona Abilidade cruz
+    for (int i = 0; i < tamanho10; i++) {
+        tabuleiro[x10 + i][y10] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+
+    for (int i = 0; i < tamanho11; i++) {
+        tabuleiro[x11][y11 + i] = AREA_AFETADA_PELA_ABILIDADE;
+    }
+}
+
+void exibirCoordenadasNavios(int x1, int y1, int tamanho1, int x2, int y2, int tamanho2, int x3, int y3, int tamanho3, int x4, int y4, int tamanho4) {
     
     // Navio vertical
     for (int i = 0; i < tamanho1; i++) {
@@ -49,7 +83,34 @@ void exibirCoordenadas(int x1, int y1, int tamanho1, int x2, int y2, int tamanho
     }
 
     // Navio diagonal 2
-    for (int i = 0; i < tamanho3; i++) {
+    for (int i = 0; i < tamanho4; i++) {
+    }
+}
+
+void exibirCordenadasAbilidades(int x5, int y5, int tamanho5, int x6, int y6, int tamanho6, int x7, int y7, int tamanho7, int x8, int y8, int tamanho8, int x9, int y9, int tamanho9, int x10, int y10, int tamanho10, int x11, int y11, int tamanho11) {
+    
+    // Abilidade cone
+    for (int i = 0; i < tamanho5; i++) {
+    }
+
+    for (int i = 0; i < tamanho6; i++) {
+    }
+
+    for (int i = 0; i < tamanho7; i++) {
+    }
+
+    // Abilidade octaedro
+    for (int i = 0; i < tamanho8; i++) {
+    }
+
+    for (int i = 0; i < tamanho9; i++) {
+    }
+
+    // Abilidade cruz
+    for (int i = 0; i < tamanho10; i++) {
+    }
+
+    for (int i = 0; i < tamanho11; i++) {
     }
 }
 
@@ -76,40 +137,25 @@ int main() {
     int x3 = 0, y3 = 6, tamanho3 = 3;  // Navio diagonal
     int x4 = 4, y4 = 3, tamanho4 = 3;  // Navio diagonal 2
 
+    // Definição das posições e tamanhos das abilidades
+    // abilidade cone
+    int x5 = 1, y5 = 4, tamanho5 = 1;
+    int x6 = 2, y6 = 3, tamanho6 = 3;
+    int x7 = 3, y7 = 2, tamanho7 = 5;
+
+    // abilidade octaedro
+    int x8 = 7, y8 = 1, tamanho8 = 3;
+    int x9 = 6, y9 = 2, tamanho9 = 3;
+
+    // abilidade cruz
+    int x10 = 5, y10 = 6, tamanho10 = 3;
+    int x11 = 6, y11 = 4, tamanho11 = 5;
+
     posicionarNavios(tabuleiro, x1, y1, tamanho1, x2, y2, tamanho2, x3, y3, tamanho3, x4, y4, tamanho4);
-    exibirCoordenadas(x1, y1, tamanho1, x2, y2, tamanho2, x3, y3, tamanho3, x4, y4, tamanho4);
+    posicionarAbilidades(tabuleiro, x5, y5, tamanho5, x6, y6, tamanho6, x7, y7, tamanho7, x8, y8, tamanho8, x9, y9, tamanho9, x10, y10, tamanho10, x11, y11, tamanho11);
+    exibirCoordenadasNavios(x1, y1, tamanho1, x2, y2, tamanho2, x3, y3, tamanho3, x4, y4, tamanho4);
+    exibirCordenadasAbilidades(x5, y5, tamanho5, x6, y6, tamanho6, x7, y7, tamanho7, x8, y8, tamanho8, x9, y9, tamanho9, x10, y10, tamanho10, x11, y11, tamanho11);
     imprimirTabuleiro(tabuleiro);
 
     return 0;
 }
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 
-
